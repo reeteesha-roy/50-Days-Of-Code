39836331685e6e454d5e7fa99169e4724d0e60d1
Reeteesha Roy <reeteesharoy@gmail.com>
2025-05-12 20:30:57 +0530

    Update leetcode1822.py

--- day13/leetcode1822.py
+++ day13/leetcode1822.py
@@ -1,1 +1,12 @@
+class Solution:
+    def arraySign(self, nums: list[int]) -> int:
+        sign = 1  # start assuming the product is positive
+
+        for num in nums:
+            if num == 0:
+                return 0  # product will definitely be zero
+            elif num < 0:
+                sign *= -1  # flip the sign
+
+        return sign
 
