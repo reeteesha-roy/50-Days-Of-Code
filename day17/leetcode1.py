9e14e49494eeb55f13233e9192886c6e7857a38b
Reeteesha Roy <reeteesharoy@gmail.com>
2025-05-16 17:18:43 +0530

    Update leetcode1.py

--- day17/leetcode1.py
+++ day17/leetcode1.py
@@ -1,1 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_map = {}  # value: index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
 
