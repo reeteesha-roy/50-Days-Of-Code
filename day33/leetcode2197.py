48b4c8c1ca0481d041623ab358aa8f20702ce0c0
Reeteesha Roy <reeteesharoy@gmail.com>
2025-06-07 01:32:01 +0530

    Update leetcode2197.py

--- day33/leetcode2197.py
+++ day33/leetcode2197.py
@@ -1,1 +1,19 @@
+import math
+
+class Solution:
+    def replaceNonCoprimes(self, nums):
+        stack = []
+
+        for num in nums:
+            # Try to merge with the top of the stack if possible
+            while stack:
+                gcd_val = math.gcd(stack[-1], num)
+                if gcd_val == 1:
+                    break  # Coprime, no merge
+                # Non-coprime: replace with LCM
+                num = (stack[-1] * num) // gcd_val
+                stack.pop()
+            stack.append(num)
+
+        return stack
 
