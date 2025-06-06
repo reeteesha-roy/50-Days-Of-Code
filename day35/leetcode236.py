e881374f62542b7392117bcc57db531914a1288d
Reeteesha Roy <reeteesharoy@gmail.com>
2025-06-07 01:21:32 +0530

    Create leetcode236.py

---
+++ day35/leetcode236.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # Base case: if root is None, or root is one of the target nodes
+        if not root or root == p or root == q:
+            return root
+        
+        # Search in left and right subtrees
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # If both left and right are not None, current node is LCA
+        if left and right:
+            return root
+
+        # Otherwise return the non-null child (either left or right)
+        return left if left else right
