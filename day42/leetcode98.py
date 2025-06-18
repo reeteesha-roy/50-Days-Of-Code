36f76ecd9ca91e9a8ef5ea315918f4d9f36401b5
Reeteesha Roy <reeteesharoy@gmail.com>
2025-06-18 11:21:46 +0530

    Create leetcode98.py

---
+++ day42/leetcode98.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            
+            return helper(node.left, lower, val) and helper(node.right, val, upper)
+        
+        return helper(root)
